02dc44b1bb40f08568c39b219a4e919df326e5e6
minhqpham <72894427+minhqpham@users.noreply.github.com>
2021-04-12 13:26:46 -0400

    change to -q

--- cross-validation.py
+++ cross-validation.py
@@ -7,15 +7,17 @@ from commonutil import *
 
 y, x = svm_read_problem('DogsVsCats/DogsVsCats.train')
 prob = svm_problem(y, x)
-
+    
+# linear
 for x in range(1,10):
-    # linear
     print("10-fold Linear Kernel - iteration #", x)
-    param = svm_parameter('-t 0 -v 10 -h 0')
+    param = svm_parameter('-t 0 -v 10 -h 0 -q')
     m = svm_train(prob, param, '-q')
-    # polynomial with degree 5
+
+# polynomial with degree 5
+for x in range(1,10):
     print("\n10-fold Polynomial Kernel - iteration #", x)
-    param = svm_parameter('-t 1 -d 5 -v 10 -h 0')
+    param = svm_parameter('-t 1 -d 5 -v 10 -h 0 -q')
     m = svm_train(prob, param, '-q')
 
 # training dataset
